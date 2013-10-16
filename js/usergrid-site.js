f59ef942fb23b321c59ca51d6512776094e2003e
Tim Anglade <timanglade@gmail.com>
2013-10-15 23:01:11 -0700

    Fix rendering bugs on xsmall screens

--- js/usergrid-site.js
+++ js/usergrid-site.js
@@ -2,6 +2,8 @@ var start = 40;
 var end = 210;
 
 function setHeaderForScroll(scrollTop) {
+  if ($( window ).width() > 768) {
+
     if(scrollTop > start) {
         opacity = (Math.floor(scrollTop) - start)/end;
         //console.log(opacity);
@@ -19,23 +21,28 @@ function setHeaderForScroll(scrollTop) {
     }
     $("#home-logo").css("opacity", opacity);
     $("header").css("box-shadow", "0px 1px "+blur+"px rgb("+red+','+green+','+blue+")");
+  } else {
+    $("#home-logo").css("opacity", 1);
+    $("header").css("box-shadow", "0px 1px 2px rgb(52,73,94)");
+  }
 }
 
 $(document).ready(function() {
 
-  // Detect initial scroll on page load
-  setHeaderForScroll($("body").scrollTop());
-
-  //reduce the opacity of the banner if the page is scrolled.
-  $(window).scroll(function () {
+    // Detect initial scroll on page load
     setHeaderForScroll($("body").scrollTop());
-  });
 
-  $(".navbar-toggle").bind("click", function(){
-      if($(".collapse").hasClass("collapse"))
-          $("#home-logo").css("opacity", 100);
-      else
-          $("#home-logo").css("opacity", 0);
-  });
+    //reduce the opacity of the banner if the page is scrolled.
+    $(window).scroll(function () {
+      setHeaderForScroll($("body").scrollTop());
+    });
+
+    $(".navbar-toggle").bind("click", function(){
+        if($(".collapse").hasClass("collapse"))
+            $("#home-logo").css("opacity", 100);
+        else
+            $("#home-logo").css("opacity", 0);
+    });
+  
 
 });
