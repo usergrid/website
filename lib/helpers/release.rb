7a9d84a78397e2ef654bf7497bafe4bdcfc0ccbe
Tim Anglade <timanglade@gmail.com>
2013-11-17 12:40:54 -0800

    Fix links

--- lib/helpers/release.rb
+++ lib/helpers/release.rb
@@ -4,13 +4,13 @@ require 'json'
 require 'date'
 
 module ReleaseHelper
-  def release(group, artifact, type)
-    file = open('http://search.maven.org/solrsearch/select?q=g:%22'+ group +'%22+AND+a:%22'+ artifact +'%22&rows=20&wt=json')
-    parsed = JSON.parse(file.read)
-    if type != 'timestamp'
-      parsed['response']['docs'][0][type]
-    else
-      DateTime.strptime(parsed['response']['docs'][0][type].to_s[0,10],'%s')
-    end
+  def releases()
+    file = open('http://search.maven.org/solrsearch/select?q=g:%22org.usergrid%22+AND+a:%22usergrid-core%22&rows=20&core=gav')
+    return JSON.parse(file.read)
+    # if type != 'timestamp'
+    #   parsed['response']['docs'][0][type]
+    # else
+    #   DateTime.strptime(parsed['response']['docs'][0][type].to_s[0,10],'%s')
+    # end
   end
 end
